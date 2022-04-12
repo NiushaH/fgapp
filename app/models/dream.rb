9af9a8baa80769d6d0746405a497e63ce7cbb549
Niusha H <35556570+NiushaH@users.noreply.github.com>
2022-04-12 14:45:18 -0600

    Alias association notes added

--- app/models/dream.rb
+++ app/models/dream.rb
@@ -1,4 +1,5 @@
 class Dream < ApplicationRecord
+    # Aliased associations that are also self-referential associations
     belongs_to :dreamer_user, :class_name => "User"
     belongs_to :funder_user, :class_name => "User"
 end
