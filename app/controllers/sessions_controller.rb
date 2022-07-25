f72713690bdd6f538aa49520c9521b093b30124e
Niusha H <35556570+NiushaH@users.noreply.github.com>
2022-07-25 14:32:17 -0600

    Navigation Menu Updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
 # Controllers used to be called "business logic"; they are the conductor of data in your application
 
     def new
+      @user = User.new
     end
 
     def create
@@ -15,8 +16,25 @@ class SessionsController < ApplicationController
       end
     end
 
+    def omniauth
+      # use byebug to inspect what the auth method returns
+      @user = User.from_omniauth(auth)
+      if @user.valid?
+        session[:user_id] = @user.id
+        redirect_to @user
+      else
+        render :new
+      end
+    end
+
     def destroy
       reset_session
       redirect_to "/"
+    end
+
+    private
+
+    def auth
+      request.env['omniauth.auth']
     end
 end
