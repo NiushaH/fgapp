8d6ea83b060e8826cbea49c7ec40212d4c3fa6f4
Niusha H <35556570+NiushaH@users.noreply.github.com>
2022-07-25 13:52:30 -0600

    add a few sessions and user routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,17 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/register', to: 'users#new'
-
   # REFACTOR RESOURCES AND RETHINK IF /FUNDS, /MYFUNDERS, and /DREAMERSISUPPORT SHOULD BE SCOPES
   # resources :users, only: [:create, :show_dreams :as => :users_dreams, :show_investments :as => :user_funds, :show_my_funders, :show_dreamers_I_support] do
   #   resources :dreams 
   post '/users', to: 'users#create'
+  get '/users/reset_password', to: 'users#reset_form'
+  post '/users/reset_password', to: 'users#password_reset'
 
+  get 'auth/:provider/callback', to: 'sessions#omniauth'
   # creates a session between a cookie and a browser
   get '/login', to: 'sessions#new'
   post '/sessions', to: 'sessions#create'
-
   get '/logout', to: 'sessions#destroy'
 
   # Rails route helper
